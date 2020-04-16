4270803de2fd2cc8916cbd185da8739be4815b66
Noname-r <noname-r@ya.ru>
2020-04-16 19:32:05 +0300

    Lesson 3 complete

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -2,7 +2,9 @@
 
 package lesson3.task1
 
-import kotlin.math.sqrt
+import lesson5.task1.whoAreInBoth
+import lesson6.task1.plusMinus
+import kotlin.math.*
 
 /**
  * Пример
@@ -12,7 +14,7 @@ import kotlin.math.sqrt
 fun factorial(n: Int): Double {
     var result = 1.0
     for (i in 1..n) {
-        result = result * i // Please do not fix in master
+        result *= i // Please do not fix in master
     }
     return result
 }
@@ -67,7 +69,16 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int {
+    var counter = 0
+    var i = 1L
+    while (i <= n) {
+        counter++
+        i *= 10
+        println(i)
+    }
+    if (n == 0) return 1 else return counter
+}
 
 /**
  * Простая
@@ -75,7 +86,21 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+//fun fib(n: Int): Int {
+//    var sumPrev = 0
+//    var sum = 1
+//    var sumNext = 0
+//    if (n == 1) return 1
+//    for (i in 2..n) {
+//        sumNext = sumPrev + sum
+//        sumPrev = sum
+//        sum = sumNext
+//    }
+//    return sumNext
+//}
+
+fun fib(n: Int): Int =
+    if (n in 1..2) 1 else fib(n - 1) + fib(n - 2)
 
 /**
  * Простая
@@ -83,21 +108,40 @@ fun fib(n: Int): Int = TODO()
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+
+fun lcm(m: Int, n: Int): Int {
+    var a = m
+    var b = n
+    if (a > b) {
+        a = n
+        b = m
+    }
+    var count = 1
+    while ((b * count) % a != 0) count++
+    return b * count
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    var count = 2
+    while (n % count != 0) count++
+    return count
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    var count = 2
+    while (n % (n / count) != 0) count++
+    return n / count
+}
 
 /**
  * Простая
@@ -106,7 +150,19 @@ fun maxDivisor(n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    if ((n < 2) || (n < 2)) return false
+    if ((m == 2) || (n == 2)) return true
+    if ((m % 2 == 0) && (n % 2 == 0)) return false
+    for (i in 3..sqrt(m.toDouble()).toInt() step 2) {
+        if (m % i == 0) {
+            if (n % i == 0) {
+                return false
+            }
+        }
+    }
+    return true
+}
 
 /**
  * Простая
@@ -115,7 +171,12 @@ fun isCoPrime(m: Int, n: Int): Boolean = TODO()
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
+fun squareBetweenExists(m: Int, n: Int): Boolean {
+    for (i in m..n) {
+        if (sqrt(i.toDouble()) == sqrt(i.toDouble()).toInt().toDouble()) return true
+    }
+    return false
+}
 
 /**
  * Средняя
@@ -133,19 +194,50 @@ fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
  * Написать функцию, которая находит, сколько шагов требуется для
  * этого для какого-либо начального X > 0.
  */
-fun collatzSteps(x: Int): Int = TODO()
+fun collatzSteps(x: Int): Int {
+    var nextX = x
+    var count = 0
+    while (nextX != 1) {
+        if (nextX % 2 == 0) nextX /= 2
+        else nextX = 3 * nextX + 1
+        count++
+    }
+    return count
+}
 
 /**
  * Средняя
  *
  * Для заданного x рассчитать с заданной точностью eps
  * sin(x) = x - x^3 / 3! + x^5 / 5! - x^7 / 7! + ...
+ * a := a * (-1) * x * x / ((i - 1) * i);
  * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю.
  * Подумайте, как добиться более быстрой сходимости ряда при больших значениях x.
  * Использовать kotlin.math.sin и другие стандартные реализации функции синуса в этой задаче запрещается.
  */
 fun sin(x: Double, eps: Double): Double = TODO()
+/*
+{
+    var result = x
+    var mult = -1
+    var count = 3
+    var next = 0.0
+    println(eps)
+    do  {
+        next = mult*x.pow(count)/(count-1)*count
+        println(next)
+        println(next.absoluteValue)
+        result += next
+        mult *= -1
+        count += 2
+    } while (next.absoluteValue >= eps)
+    return result
+}
+*/
 
+fun main() {
+    sin(PI / 2.0, 1e-5)
+}
 /**
  * Средняя
  *
@@ -164,7 +256,15 @@ fun cos(x: Double, eps: Double): Double = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun revert(n: Int): Int = TODO()
+fun revert(n: Int): Int {
+    var _n = n
+    var result = 0
+    while (_n > 0) {
+        result = result * 10 + _n % 10
+        _n /= 10
+    }
+    return result
+}
 
 /**
  * Средняя
@@ -175,7 +275,23 @@ fun revert(n: Int): Int = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun isPalindrome(n: Int): Boolean = TODO()
+fun isPalindrome(n: Int): Boolean {
+    var _n = n
+    var first = 0
+    var count = 0
+    while (_n > 0) {
+        _n = _n / 10
+        count++
+    }
+    _n = n
+    while (count > 1) {
+        first = (_n / (10.0.pow(count - 1)).toInt()) % 10
+        if (first != _n % 10) return false
+        _n = _n % (10.0.pow(count - 1)).toInt() / 10
+        count -= 2
+    }
+    return true
+}
 
 /**
  * Средняя
@@ -185,7 +301,15 @@ fun isPalindrome(n: Int): Boolean = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun hasDifferentDigits(n: Int): Boolean = TODO()
+fun hasDifferentDigits(n: Int): Boolean {
+    var prev = n % 10
+    var _n = n / 10
+    while (_n > 0) {
+        if (prev != _n % 10) return true
+        _n /= 10
+    }
+    return false
+}
 
 /**
  * Сложная
@@ -196,7 +320,25 @@ fun hasDifferentDigits(n: Int): Boolean = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun squareSequenceDigit(n: Int): Int = TODO()
+fun squareSequenceDigit(n: Int): Int {
+    var num = 0
+    var count = 0
+    while (count < n) {
+        num++
+        when (num * num) {
+            in 1..9 -> count += 1
+            in 10..99 -> count += 2
+            in 100..999 -> count += 3
+            in 1000..9999 -> count += 4
+        }
+    }
+    var lastSqr = num * num
+    while (count != n) {
+        lastSqr /= 10
+        count--
+    }
+    return lastSqr % 10
+}
 
 /**
  * Сложная
@@ -207,4 +349,23 @@ fun squareSequenceDigit(n: Int): Int = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun fibSequenceDigit(n: Int): Int = TODO()
+fun fibSequenceDigit(n: Int): Int {
+    var num = 0
+    var count = 0
+    while (count<n) {
+        num++
+        when (fib(num)) {
+            in 1..9 -> count += 1
+            in 10..99 -> count += 2
+            in 100..999 -> count += 3
+            in 1000..9999 -> count += 4
+        }
+    }
+    var lastSqr = fib(num)
+    while (count != n) {
+        lastSqr /= 10
+        count--
+    }
+    return lastSqr % 10
+}
+
--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -136,7 +136,8 @@ class Tests {
         assertTrue(isCoPrime(25, 49))
         assertFalse(isCoPrime(6, 8))
         assertTrue(isCoPrime(17, 97))
-        assertFalse(isCoPrime(37, 111))
+        assertTrue(isCoPrime(37, 111))
+        assertTrue(isCoPrime(111, 37))
         assertTrue(isCoPrime(1234567890, 908765431))
         assertTrue(isCoPrime(2109876543, 1234567891))
     }
